183f68e9d8ddb856d2be45f03e87d76da231f70d
Andrey Marchenko <andrey.marchenko@datadoghq.com>
2024-04-19 13:20:38 +0200

    fix failing test

--- spec/rubocop/options_spec.rb
+++ spec/rubocop/options_spec.rb
@@ -27,10 +27,10 @@ RSpec.describe RuboCop::Options, :isolated_environment do
       end
 
       it 'shows help text' do
+
         begin
           options.parse(['--help'])
         rescue SystemExit # rubocop:disable Lint/SuppressedException
-          p "fd"
         end
 
         # rubocop:todo Naming/InclusiveLanguage
